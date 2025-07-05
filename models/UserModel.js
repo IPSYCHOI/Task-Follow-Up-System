4a81b73868c4a9a1a3c6c94dc108eb2db4910a00
psycho <aali571110@gmail.com>
2025-07-05 04:06:23 +0300

    create user schema

---
+++ models/UserModel.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose"
+import bcrypt from "bcrypt"
+
+const userSchema=new mongoose.Schema({
+    email:{
+        type:String,
+        unique:true,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+},
+{
+    timestamps:true
+}
+)
+
+userSchema.pre("save",async function (next){
+    if(!this.isModified("password"))
+        return next()
+
+    try {
+        this.password=await bcrypt.hash(this.password,12)
+        next()
+    } catch (error) {
+        next(error)
+    }
+})
+
+userSchema.methods.passCheck=function(password){
+    return bcrypt.compare(  password,this.password)
+}
+module.exports = mongoose.model('User', userSchema)
