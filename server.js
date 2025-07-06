8b3dc3b4f7150591b85cd15b0abc8ea4822860ac
psycho <132718283+IPSYCHOI@users.noreply.github.com>
2025-07-06 11:30:27 +0300

    edit package.json (#8)

--- server.js
+++ server.js
@@ -4,11 +4,9 @@ import dbconnect from "./config/dbConnect.js"
 
 config()
 const port = process.env.PORT || 3000;
-const environment =process.env.NODE_ENV || 'development'
 dbconnect()
     .then(()=>{
         app.listen(port,() => console.log("🚀 Server running on port "+port))
-        console.log("Environment: "+environment);
     })
     .catch((err) => {
         console.error("❌ Failed to connect DB or start server:", err.message);
