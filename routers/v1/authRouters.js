6ca82c1df9896d04f810c0d52d5470466ca17ef5
psycho <aali571110@gmail.com>
2025-07-07 02:40:16 +0300

    auth middlewares

--- routers/v1/authRouters.js
+++ routers/v1/authRouters.js
@@ -1,12 +1,8 @@
 import express from 'express'
 const router = express.Router();
 import userController from '../../controllers/v1/userController.js'
-import { isAuth } from '../../middlewares/authMiddleware.js';
-
 
 router.post('/login',userController.login);
 router.post('/signup',userController.signUp);
-
-
 
 export default router
--- utils/generateJwt.js
+++ utils/generateJwt.js
@@ -1,7 +1,9 @@
 import jwt  from "jsonwebtoken";
 import { config } from "dotenv";
 config()
+
+const JWT_SECRET = process.env.JWT_SECRET
 export const generateToken=(userId)=>{
-    const token = jwt.sign({id:userId},process.env.JWT_SECRET,{expiresIn:"30m"})
+    const token = jwt.sign({id:userId},JWT_SECRET,{expiresIn:"1d"})
     return token
 }
