b40f57fe19640fe4e21d4e35154bdcf2b603fb95
psycho <132718283+IPSYCHOI@users.noreply.github.com>
2025-07-07 17:40:45 +0300

    Feat/auth (#18)
    * edit package.json
    
    * dev script
    
    * login
    
    * signUp
    
    * merge dev
    
    * auth middlewares
    
    * tasks attribute
    
    * soft delete
    
    * softDelete end point
    
    * meeting

--- routers/v1/index.js
+++ routers/v1/index.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import authRouters from "./authRouters.js"
+import {taskRouter}  from './taskRouter.js'
 const router = express.Router()
 
+router.use("/task",taskRouter)
 router.use("/auth",authRouters)
 
 export default router
