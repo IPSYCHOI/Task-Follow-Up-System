b3b5e2e3bc2b0cdac4185e68200e5df4aa9a6b79
aelaraby6 <body1262005@gmail.com>
2025-07-08 22:29:13 +0300

    Update-Task-CRUD

--- routers/v1/taskRouter.js
+++ routers/v1/taskRouter.js
@@ -1,19 +1,19 @@
 import { Router } from 'express';
 import { GetAllTasks } from '../../controllers/v1/dashboardController.js';
-import { GetAllTasksController } from '../../controllers/v1/taskControllers/getTasksController.js';
-import { GetTaskController } from '../../controllers/v1/taskControllers/getTaskController.js';
-import { DeleteTaskController } from '../../controllers/v1/taskControllers/deleteTaskController.js';
-import { CreateTaskController } from '../../controllers/v1/taskControllers/createTaskController.js';
-import { UpdateTaskController } from '../../controllers/v1/taskControllers/updateTaskController.js';
+import { GetAllTasksController } from '../../controllers/v1/getTasksController.js';
+import { GetTaskController } from '../../controllers/v1/getTaskController.js';
+import { DeleteTaskController } from '../../controllers/v1/deleteTaskController.js';
+import { CreateTaskController } from '../../controllers/v1/createTaskController.js';
+import { UpdateTaskController } from '../../controllers/v1/updateTaskController.js';
 import { isAuth } from '../../middlewares/authMiddleware.js';
 
 const router = Router();
 
 router.get('/dashboard', isAuth, GetAllTasks);
-router.get('/task', isAuth, GetAllTasksController);
-router.get('/task/:id', isAuth, GetTaskController);
-router.delete('/task/:id', isAuth, DeleteTaskController);
-router.post('/task', isAuth, CreateTaskController);
-router.put('/task:id', isAuth, UpdateTaskController);
+router.get('/tasks', isAuth, GetAllTasksController);
+router.get('/tasks/:id', isAuth, GetTaskController);
+router.delete('/tasks/:id', isAuth, DeleteTaskController);
+router.post('/tasks', isAuth, CreateTaskController);
+router.put('/tasks:id', isAuth, UpdateTaskController);
 
 export { router as taskRouter };
