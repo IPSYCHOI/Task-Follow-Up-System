0f15a5d58ecbac13858a6c45bb088d7d543b5ffe
psycho <aali571110@gmail.com>
2025-07-11 06:51:10 +0300

    add method in not found handler

--- middlewares/notFoundHandler.js
+++ middlewares/notFoundHandler.js
@@ -1,5 +1,5 @@
 import { NotFoundError } from "../Errors/error.js";
 export const notFound=(req,res,next)=>{
-    next(new NotFoundError("That Route " +req.url+ " not found"))
+    next(new NotFoundError("That Route " +req.url+ "with method "+req.method+" not found"))
 }
 export default notFound
